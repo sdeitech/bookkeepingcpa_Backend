c89cd45ac7e009fb498d69c994e5ae37cd28c4ce
Vivek Singh Sisodiya <viveksinghsisodiya@smartdatainc.net>
2026-01-27 11:03:40 +0530

    Fix Zapier Webhook URL

--- api/services/zapier.services.js
+++ api/services/zapier.services.js
@@ -1,10 +1,8 @@
 const axios = require("axios");
 
-const ZAPIER_WEBHOOK_URL =
-  "https://hooks.zapier.com/hooks/catch/26160548/uqsg27t/";
 
 const sendToZapier = async (payload) => {
-  return axios.post(ZAPIER_WEBHOOK_URL, payload, {
+  return axios.post(process.env.ZAPIER_WEBHOOK_URL, payload, {
     headers: {
       "Content-Type": "application/json",
     },
